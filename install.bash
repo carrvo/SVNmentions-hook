c719334fb5d4bda56376d364748cda9107c698ff
carrvo <carrvo@gmail.com>
2025-04-08 10:20:42 -0600

    post to receiver

--- install.bash
+++ install.bash
@@ -22,15 +22,28 @@ then
 	exit 1
 fi
 
+if [ "$3" == "" ];
+then
+    CLIENT_ID=WebmentionsHook
+else
+    CLIENT_ID="$3"
+fi
+
 pushd "$1" > /dev/null
 cd hooks/
 echo installing post-commit in $PWD
 ln -s $SCRIPT_DIR/post-commit
 echo "$2" > $PWD/apache2.path
+echo "$CLIENT_ID" > $PWD/apache2.id
 if [ ! -f $PWD/apache2.path ];
 then
     echo "Please manually create apache2.path"
     echo "echo $2 > $PWD/apache2.path"
+fi
+if [ ! -f $PWD/apache2.id ];
+then
+    echo "Please manually create apache2.id"
+    echo "echo $CLIENT_ID > $PWD/apache2.id"
 fi
 popd > /dev/null
 
