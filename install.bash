5b5ba5aec54200f5d171b85e16edeceb6baac4f4
carrvo <carrvo@gmail.com>
2025-04-07 18:36:59 -0600

    implement hook side

--- install.bash
+++ install.bash
@@ -3,9 +3,22 @@
 # Credit: https://stackoverflow.com/a/246128
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ "$1" == "" ];
+# ensure:
+# 1) has a value
+# 2) ends with slash (/)
+if [[ "$1" == "" || "$1" != */ ]];
 then
-	echo "usage: ./install.bash /path/to/svn-repo/"
+	echo "usage: ./install.bash /path/to/svn-repo/ https://example.com/apache2/webspace/path"
+	exit 1
+fi
+
+# ensure:
+# 1) has a value
+# 2) starts with http (for http:// or https://)
+# 3) does NOT end with slash (/)
+if [[ "$2" == "" || "$2" != http* || "$2" == */ ]];
+then
+	echo "usage: ./install.bash /path/to/svn-repo/ https://example.com/apache2/webspace/path"
 	exit 1
 fi
 
@@ -13,5 +26,11 @@ pushd "$1" > /dev/null
 cd hooks/
 echo installing post-commit in $PWD
 ln -s $SCRIPT_DIR/post-commit
+echo "$2" > $PWD/apache2.path
+if [ ! -f $PWD/apache2.path ];
+then
+    echo "Please manually create apache2.path"
+    echo "echo $2 > $PWD/apache2.path"
+fi
 popd > /dev/null
 
